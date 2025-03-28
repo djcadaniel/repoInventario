6ad175deab521465a29c77eef841df5ffcb19857
djcadaniel <djcadaniel@gmail.com>
2025-03-28 14:38:05 -0500

    add settings

--- appInventario/settings.py
+++ appInventario/settings.py
@@ -144,10 +144,9 @@ USE_TZ = True
 
 # /*************/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-  os.path.join(BASE_DIR, 'static')
-]
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_DIRS = 'whitenoise.storage.CompressedManisfestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
