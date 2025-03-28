e9efbf2ddb8e966d7e20510fee6028e43ca7e502
djcadaniel <djcadaniel@gmail.com>
2025-03-28 12:52:11 -0500

    add estilos

--- appInventario/settings.py
+++ appInventario/settings.py
@@ -144,6 +144,10 @@ USE_TZ = True
 
 # /*************/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticsfiles')
+STATICFILES_DIRS = [
+  os.path.join(BASE_DIR, 'static')
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
