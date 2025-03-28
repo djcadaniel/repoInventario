00f782d636a40f3a8718a34ec5c92f0e39503015
djcadaniel <djcadaniel@gmail.com>
2025-03-28 11:38:14 -0500

    editing web sevices in setting

--- appInventarioINEL/appInventario/settings.py
+++ appInventarioINEL/appInventario/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z1wq%^wpl!e^^%t)4pu7jw$4w!1n0dq3@1h_2^n(o!$hml5@-m'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -107,7 +107,8 @@ DATABASES = {
     }
 }
 
-DATABASES["default"] = dj_database_url.parse("postgresql://bdinventario_user:xU4huxtjoJOcyN5Twe2InUNWxXWU2cA1@dpg-cvjbd5p5pdvs73daolp0-a.oregon-postgres.render.com/bdinventario")
+database_url = os.environ.get("DATABASE_URL")
+DATABASES["default"] = dj_database_url.parse(database_url)
 
 
 # Password validation
